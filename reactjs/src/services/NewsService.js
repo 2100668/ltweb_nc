31bcdbc9a694c59a6deb97e56d94280d7bd742bf
phatvan287 <phat90735@gmail.com>
2024-12-03 21:29:44 +0700

    bk3

--- reactjs/src/services/NewsService.js
+++ reactjs/src/services/NewsService.js
@@ -7,3 +7,4 @@ export const fetchNewsById = (id) => axios.get(`${API_URL}/${id}`);
 export const createNews = (data) => axios.post(API_URL, data);
 export const updateNews = (id, data) => axios.put(`${API_URL}/${id}`, data);
 export const deleteNews = (id) => axios.delete(`${API_URL}/${id}`);
+ 123
